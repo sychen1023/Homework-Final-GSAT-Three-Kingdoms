234bef827ee0b110e8cd61ecffd24f724a6bb64e
Shiuan-Ye <sychen1023@gmail.com>
2025-12-19 01:53:06 +0800

    Initial Commit

---
+++ Final Project 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project 2
+//
+//  Created by 陳宣燁 on 2025/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final Project 2/Final_Project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_2App.swift
+//  Final Project 2
+//
+//  Created by 陳宣燁 on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
